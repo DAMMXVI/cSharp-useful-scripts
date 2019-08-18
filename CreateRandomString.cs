3e39dace7c330ada876ed758d68420e7a08670fb
Alper <48470323+alpdnz@users.noreply.github.com>
2019-08-18 17:40:23 +0300

    Update CreateRandomString.cs

--- CreateRandomString.cs
+++ CreateRandomString.cs
@@ -12,5 +12,5 @@ namespace Web_Programming_Project.Methods
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
         }
-	}
+    }
 }
