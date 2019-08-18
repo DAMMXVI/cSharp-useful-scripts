8a5097af703185e117114f1d99b1bbfb37605aad
Alper <alpdnz@users.noreply.github.com>
2019-08-18 17:39:00 +0300

    Create Random String, for example password

---
+++ CreateRandomString.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Net.Mail;
+
+namespace Web_Programming_Project.Methods
+{
+    public class CreateRandomString 
+    {
+        private static Random random = new Random();
+        public static string randomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+	}
+}
