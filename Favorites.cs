ff0d5251ef25ac53a918f951423333f96b56c79d
Alper <alpdnz@users.noreply.github.com>
2019-08-18 17:20:42 +0300

    Create Favorites.cs

---
+++ Favorites.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Net.Mail;
+
+namespace Web_Programming_Project.Methods
+{
+    public class Favorites 
+    {
+        public static bool sendMail(string toMail, string subject, string body)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage("mailiniz@gmail.com", toMail, subject, body);
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Credentials = new System.Net.NetworkCredential("mailiniz@gmail.com", "şifreniz");
+                client.EnableSsl = true;
+                client.Send(mail);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
