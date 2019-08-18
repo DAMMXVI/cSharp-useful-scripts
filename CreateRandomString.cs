3716f6accd9771164e251d3c4ad894569ec59cb5
Alper <alpdnz@users.noreply.github.com>
2019-08-18 17:39:36 +0300

    Create Random String for Password

--- CreateRandomString.cs
+++ CreateRandomString.cs
@@ -2,7 +2,7 @@ using System;
 using System.Linq;
 using System.Net.Mail;
 
-namespace Web_Programming_Project.Methods
+namespace Web_Programming_Project.Methods 
 {
     public class CreateRandomString 
     {
